020da494e0a776ca464045347a9d69771418ccd6
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-10-26 16:05:36 -0300

    feat: created the service responsible for request

--- src/services/tokenCreateService.ts
+++ src/services/tokenCreateService.ts
@@ -1,21 +1,59 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config()
 interface IResponse {
-    message: string | {
-        token: string;
-        data_criacao: string;
-    };
+    message: string
+    token: string;
+    refresh_token: string;
+    data_criacao: string;
     status: number
 }
-export class CreateTokenService{
-    public async execute():Promise<IResponse>{
+export class CreateTokenService {
+    public async execute(): Promise<IResponse> {
         try {
-            return{
-                message: '',
-                status: 500
+            const cnpj = process.env.CNPJ_SH
+            const token = process.env.TOKEN_SH
+            const url = process.env.URL
+            const data = {
+                cnpj_sh: cnpj,
+                token_sh: token
+            }
+            axios.post(url + 'auth', data)
+                .then((response) => {
+                    const dataNow = new Date()
+                    const { token, refresh_token } = response.data
+                    return {
+                        message:'',
+                        token: token,
+                        refresh_token: refresh_token,
+                        data_criacao: dataNow,
+                        status: 200
+                    }
+                })
+                .catch((error) => {
+                    return {
+                        message: 'Requisition error on tecnoSpeed ',
+                        status: error.status,
+                        token: '',
+                        refresh_token: '',
+                        data_criacao: ''
+                    }
+                })
+            return {
+                message: 'Internal server Error',
+                status: 500,
+                token: '',
+                refresh_token: '',
+                data_criacao: ''
             }
         } catch (error) {
-            return{
-                message: '',
-                status: 500
+            return {
+                message: 'Internal server Error',
+                status: 500,
+                token: '',
+                refresh_token: '',
+                data_criacao: ''
             }
         }
     }

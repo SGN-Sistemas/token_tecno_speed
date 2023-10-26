c85ceb94761a2cc8d1f8a44b0d5cd683f112fec3
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-10-26 15:28:44 -0300

    feat: add function for a verify time token on controllers

--- src/controllers/tokenControllers.ts
+++ src/controllers/tokenControllers.ts
@@ -1,8 +1,23 @@
 import { Request, Response } from "express";
 import AppError from "../middlewares/AppError";
+import { VerifyToken } from "../services/verifyTimeTokenService";
+import { CreateTokenService } from "../services/tokenCreateService";
 
 export class TokenControllers {
     public async createToken(request: Request, response: Response):Promise<Response>{
-        throw new AppError('test',500)
+        const verifyToken = new VerifyToken()
+        const { status,message } = await verifyToken.execute()
+        console.log('====================================');
+        console.log(status,message);
+        console.log('====================================');
+        if(status === 200){
+            response.status(status).json(message)
+        }
+        if(status === 500){
+            throw new AppError(message,status)
+        }
+
+        return response.status(status).json({message})
+
     }
 }

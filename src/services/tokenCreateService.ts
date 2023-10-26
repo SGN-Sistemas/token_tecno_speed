c7303bf701c810a1bfaabb764d518e8a034007fa
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-10-26 15:29:02 -0300

    feat: init function create token

--- src/services/tokenCreateService.ts
+++ src/services/tokenCreateService.ts
@@ -1,5 +1,22 @@
-export class CreateToken{
-    public async execute(){
-        
+interface IResponse {
+    message: string | {
+        token: string;
+        data_criacao: string;
+    };
+    status: number
+}
+export class CreateTokenService{
+    public async execute():Promise<IResponse>{
+        try {
+            return{
+                message: '',
+                status: 500
+            }
+        } catch (error) {
+            return{
+                message: '',
+                status: 500
+            }
+        }
     }
 }

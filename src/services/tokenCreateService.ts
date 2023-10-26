0bff000067dab204fd736bd8d39897a546d8f9be
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-10-26 16:38:24 -0300

    feat: configuration to return datas

--- src/services/tokenCreateService.ts
+++ src/services/tokenCreateService.ts
@@ -14,39 +14,40 @@ export class CreateTokenService {
     public async execute(): Promise<IResponse> {
         try {
             const cnpj = process.env.CNPJ_SH
-            const token = process.env.TOKEN_SH
-            const url = process.env.URL
+            const token_sh = process.env.TOKEN_SH
+            const url = process.env.URL;
+            let token_tec: string = '';
+            let refresh_token_tec: string = '';
+            let data_criacao: string = '';
+            let status: number = 500;
+            let message: string = '';
             const data = {
                 cnpj_sh: cnpj,
-                token_sh: token
+                token_sh
             }
-            axios.post(url + 'auth', data)
+            await axios.post(url + 'auth', data)
                 .then((response) => {
                     const dataNow = new Date()
                     const { token, refresh_token } = response.data
-                    return {
-                        message:'',
-                        token: token,
-                        refresh_token: refresh_token,
-                        data_criacao: formatDbData(dataNow),
-                        status: 200
-                    }
+                    message = '';
+                    status = 200;
+                    token_tec = token;
+                    refresh_token_tec = refresh_token;
+                    data_criacao = formatDbData(dataNow);
                 })
                 .catch((error) => {
-                    return {
-                        message: 'Requisition error on tecnoSpeed ',
-                        status: error.status,
-                        token: '',
-                        refresh_token: '',
-                        data_criacao: ''
-                    }
+                    message = 'Requisition error on tecnoSpeed ';
+                    status = error.status;
+                    token_tec = '';
+                    refresh_token_tec = '';
+                    data_criacao = '';
                 })
             return {
-                message: 'Internal server Error',
-                status: 500,
-                token: '',
-                refresh_token: '',
-                data_criacao: ''
+                message,
+                status,
+                token: token_tec,
+                refresh_token: refresh_token_tec,
+                data_criacao: data_criacao
             }
         } catch (error) {
             return {

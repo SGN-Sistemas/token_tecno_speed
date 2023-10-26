1fecde84f7b346e55d7de549c38fde4a9050b8b3
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-10-26 10:57:35 -0300

    feat: add configuration cors and use for AppError configurantion

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,27 @@
 import 'express-async-errors'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
+import AppError from './middlewares/AppError'
 
 const app = express()
+
 const port = 9006
 
-app.listen(port,()=>{
+app.use(cors())
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+        return res.status(error.statusCode).json({
+            error: error.message,
+            status: error.statusCode
+        })
+    }
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server'
+    })
+})
+
+app.listen(port, () => {
     console.log('====================================');
     console.log('Rodando na porta ' + port);
     console.log('====================================');

f2860182febe28ef32ff7e1e17c0c0e1fe774d1d
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-10-26 16:37:20 -0300

    feat: add hour minutes seconds for function

--- src/utils/transformData.ts
+++ src/utils/transformData.ts
@@ -2,5 +2,8 @@ export function formatDbData(data: Date) {
     const year = data.getFullYear();
     const month = String(data.getMonth() + 1).padStart(2, '0');
     const day = String(data.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
+    const hour = String(data.getHours()).padStart(2, '0');
+    const minutes = String(data.getMinutes()).padStart(2, '0');
+    const seconds = String(data.getSeconds()).padStart(2, '0');
+    return `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`;
 }

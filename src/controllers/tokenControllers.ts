2a9a019af8f3907f5d1653aea56389a09f9ec5cf
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-10-26 16:10:06 -0300

    feat: implements configuration  for created token

--- src/controllers/tokenControllers.ts
+++ src/controllers/tokenControllers.ts
@@ -5,6 +5,9 @@ import { CreateTokenService } from "../services/tokenCreateService";
 
 export class TokenControllers {
     public async createToken(request: Request, response: Response):Promise<Response>{
+        let token: string;
+        let refresh_token:string;
+        let data_criacao: string;
         const verifyToken = new VerifyToken()
         const { status,message } = await verifyToken.execute()
         console.log('====================================');
@@ -16,9 +19,17 @@ export class TokenControllers {
         if(status === 500){
             throw new AppError(message,status)
         }
+        if(status === 204){
+            const createTokenService = new CreateTokenService()
+            const createTokenServiceExec = await createTokenService.execute()
+            if(createTokenServiceExec.status === 500 ){
+                throw new AppError(createTokenServiceExec.message,createTokenServiceExec.status)
+            }
+            token   =   createTokenServiceExec.token
+            refresh_token = createTokenServiceExec.refresh_token
+            data_criacao = createTokenServiceExec.data_criacao
+        }
 
-        const createTokenService = new CreateTokenService()
-        const createTokenServiceExec = await createTokenService.execute()
 
         return response.status(status).json({message})
 
